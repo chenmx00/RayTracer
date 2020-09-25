e60175c9694c7fe2f196a17d523401282c2bc23d
chenmx00 <chenmx00@berkeley.edu>
2020-09-24 23:33:35 -0700

    first commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by Minxing Chen on 8/26/20.
+//  Copyright © 2020 Minxing Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    const int image_width = 256;
+    const int image_height = 256;
+    
+    for(int j = image_height -1; j >=0; j--){
+        
+    }
+}
